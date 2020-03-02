fbbd49f857902c90dece5afc00203c026e83bca1
Stig B. Dørmænen <stigbd@gmail.com>
2020-03-02 09:41:25 +0100

    Add version attribute

--- openapiparser/openapi.py
+++ openapiparser/openapi.py
@@ -2,11 +2,6 @@ import requests
 import yaml
 
 
-class Error(Exception):
-    """Base class for exceptins in this module"""
-    pass
-
-
 class OpenAPI:
     """ A simple class representing an openAPI specification"""
 
@@ -16,6 +11,29 @@ class OpenAPI:
             resp = requests.get(url)
         except requests.exceptions.ConnectionError:
             raise Error("ConnectionError")
+        if not resp:
+            raise RequestError(resp.status_code)
         spec = yaml.safe_load(resp.text)
         if spec is None:
             raise Error("Spec is not yaml")
+        self._version = spec['openapi']
+
+    @property
+    def version(self):
+        return self._version
+
+
+class Error(Exception):
+    """Base class for exceptins in this module"""
+    pass
+
+
+class RequestError(Error):
+    """The url is not found on server
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message):
+        self.message = message
--- tests/test_openapiparser.py
+++ tests/test_openapiparser.py
@@ -1,23 +1,32 @@
 import pytest
-from openapiparser.openapi import OpenAPI
-
-
-def test_read_valid_spec_from_url():
-    url = "https://raw.githubusercontent.com/Informasjonsforvaltning/"
-    "dataservice-publisher/master/dataservice-catalog.yaml"
-    api = OpenAPI(url)
-
-    assert api is not None
+from openapiparser.openapi import OpenAPI, Error, RequestError
 
 
 def test_read_spec_from_wrong_url_should_raise_error():
-    with pytest.raises(Exception):
+    with pytest.raises(Error):
         OpenAPI("http://wrong_url")
 
 
 def test_read_empty_spec_shold_raise_error():
-    with pytest.raises(Exception):
+    with pytest.raises(Error):
         OpenAPI(
-               "https://raw.githubusercontent.com/Informasjonsforvaltning"
-               "/openapiparser/master/tests/empty_spec.yaml"
+                (
+                 "https://raw.githubusercontent.com/Informasjonsforvaltning"
+                 "/openapiparser/master/tests/empty_spec.yaml"
                 )
+                )
+
+
+def test_read_spec_shold_raise_RequestError():
+    with pytest.raises(RequestError):
+        OpenAPI("https://raw.githubusercontent.com/Informasjonsforvaltning")
+
+
+def test_get_version_should_return_valid_version_string():
+    url = (
+          "https://raw.githubusercontent.com/Informasjonsforvaltning/"
+          "dataservice-publisher/master/dataservice-catalog.yaml"
+          )
+    api = OpenAPI(url)
+
+    assert api.version[:4] == "3.0."  # We support all 3.0.x versions
