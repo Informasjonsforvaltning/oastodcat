3069aaa3e4e0d844ac794ff900178b960c9cf2ba
Stig B. Dørmænen <stigbd@gmail.com>
2020-03-02 08:57:00 +0100

    Add test for empty spec

--- tests/test_openapiparser.py
+++ tests/test_openapiparser.py
@@ -10,6 +10,14 @@ def test_read_valid_spec_from_url():
     assert api is not None
 
 
-def test_read_valid_spec_from_wrong_url_should_raise_error():
+def test_read_spec_from_wrong_url_should_raise_error():
     with pytest.raises(Exception):
         OpenAPI("http://wrong_url")
+
+
+def test_read_empty_spec_shold_raise_error():
+    with pytest.raises(Exception):
+        OpenAPI(
+               "https://raw.githubusercontent.com/Informasjonsforvaltning"
+               "/openapiparser/master/tests/empty_spec.yaml"
+                )
